c6d91c24285c65ff1e4995f2a408881064ca5ac4
iHOPE <THEDEAD40@users.noreply.github.com>
2020-01-13 02:22:55 +0100

    TicToc using C#

---
+++ game/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.A00 = new System.Windows.Forms.Button();
+            this.A01 = new System.Windows.Forms.Button();
+            this.A02 = new System.Windows.Forms.Button();
+            this.A10 = new System.Windows.Forms.Button();
+            this.A11 = new System.Windows.Forms.Button();
+            this.A12 = new System.Windows.Forms.Button();
+            this.A22 = new System.Windows.Forms.Button();
+            this.A21 = new System.Windows.Forms.Button();
+            this.A20 = new System.Windows.Forms.Button();
+            this.NGB = new System.Windows.Forms.Button();
+            this.REB = new System.Windows.Forms.Button();
+            this.exB = new System.Windows.Forms.Button();
+            this.DRAW = new System.Windows.Forms.Label();
+            this.Xwin = new System.Windows.Forms.Label();
+            this.Owin = new System.Windows.Forms.Label();
+            this.CKM = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // A00
+            // 
+            this.A00.Location = new System.Drawing.Point(42, 36);
+            this.A00.Name = "A00";
+            this.A00.Size = new System.Drawing.Size(137, 130);
+            this.A00.TabIndex = 0;
+            this.A00.UseVisualStyleBackColor = true;
+            this.A00.Click += new System.EventHandler(this.buttonclick);
+            // 
+            // A01
+            // 
+            this.A01.Location = new System.Drawing.Point(220, 36);
+            this.A01.Name = "A01";
+            this.A01.Size = new System.Drawing.Size(134, 130);
+            this.A01.TabIndex = 1;
+            this.A01.UseVisualStyleBackColor = true;
+            this.A01.Click += new System.EventHandler(this.buttonclick);
+            // 
+            // A02
+            // 
+            this.A02.Location = new System.Drawing.Point(399, 36);
+            this.A02.Name = "A02";
+            this.A02.Size = new System.Drawing.Size(130, 130);
+            this.A02.TabIndex = 2;
+            this.A02.UseVisualStyleBackColor = true;
+            this.A02.Click += new System.EventHandler(this.buttonclick);
+            // 
+            // A10
+            // 
+            this.A10.Location = new System.Drawing.Point(42, 182);
+            this.A10.Name = "A10";
+            this.A10.Size = new System.Drawing.Size(137, 131);
+            this.A10.TabIndex = 3;
+            this.A10.UseVisualStyleBackColor = true;
+            this.A10.Click += new System.EventHandler(this.buttonclick);
+            // 
+            // A11
+            // 
+            this.A11.Location = new System.Drawing.Point(220, 182);
+            this.A11.Name = "A11";
+            this.A11.Size = new System.Drawing.Size(134, 131);
+            this.A11.TabIndex = 4;
+            this.A11.UseVisualStyleBackColor = true;
+            this.A11.Click += new System.EventHandler(this.buttonclick);
+            // 
+            // A12
+            // 
+            this.A12.Location = new System.Drawing.Point(399, 182);
+            this.A12.Name = "A12";
+            this.A12.Size = new System.Drawing.Size(130, 131);
+            this.A12.TabIndex = 5;
+            this.A12.UseVisualStyleBackColor = true;
+            this.A12.Click += new System.EventHandler(this.buttonclick);
+            // 
+            // A22
+            // 
+            this.A22.Location = new System.Drawing.Point(399, 333);
+            this.A22.Name = "A22";
+            this.A22.Size = new System.Drawing.Size(130, 121);
+            this.A22.TabIndex = 6;
+            this.A22.UseVisualStyleBackColor = true;
+            this.A22.Click += new System.EventHandler(this.buttonclick);
+            // 
+            // A21
+            // 
+            this.A21.Location = new System.Drawing.Point(220, 333);
+            this.A21.Name = "A21";
+            this.A21.Size = new System.Drawing.Size(134, 121);
+            this.A21.TabIndex = 7;
+            this.A21.UseVisualStyleBackColor = true;
+            this.A21.Click += new System.EventHandler(this.buttonclick);
+            // 
+            // A20
+            // 
+            this.A20.Location = new System.Drawing.Point(42, 333);
+            this.A20.Name = "A20";
+            this.A20.Size = new System.Drawing.Size(137, 121);
+            this.A20.TabIndex = 8;
+            this.A20.UseVisualStyleBackColor = true;
+            this.A20.Click += new System.EventHandler(this.buttonclick);
+            // 
+            // NGB
+            // 
+            this.NGB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NGB.Location = new System.Drawing.Point(42, 570);
+            this.NGB.Name = "NGB";
+            this.NGB.Size = new System.Drawing.Size(137, 45);
+            this.NGB.TabIndex = 10;
+            this.NGB.Text = "new game";
+            this.NGB.UseVisualStyleBackColor = true;
+            this.NGB.Click += new System.EventHandler(this.NGB_Click);
+            // 
+            // REB
+            // 
+            this.REB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.REB.Location = new System.Drawing.Point(217, 570);
+            this.REB.Name = "REB";
+            this.REB.Size = new System.Drawing.Size(137, 45);
+            this.REB.TabIndex = 11;
+            this.REB.Text = "REST";
+            this.REB.UseVisualStyleBackColor = true;
+            this.REB.Click += new System.EventHandler(this.REB_Click);
+            // 
+            // exB
+            // 
+            this.exB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exB.Location = new System.Drawing.Point(613, 570);
+            this.exB.Name = "exB";
+            this.exB.Size = new System.Drawing.Size(137, 45);
+            this.exB.TabIndex = 12;
+            this.exB.Text = "EXIT";
+            this.exB.UseVisualStyleBackColor = true;
+            this.exB.Click += new System.EventHandler(this.exB_Click);
+            // 
+            // DRAW
+            // 
+            this.DRAW.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.DRAW.Location = new System.Drawing.Point(619, 205);
+            this.DRAW.Name = "DRAW";
+            this.DRAW.Size = new System.Drawing.Size(131, 39);
+            this.DRAW.TabIndex = 13;
+            this.DRAW.Text = "label1";
+            this.DRAW.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Xwin
+            // 
+            this.Xwin.ForeColor = System.Drawing.Color.ForestGreen;
+            this.Xwin.Location = new System.Drawing.Point(619, 45);
+            this.Xwin.Name = "Xwin";
+            this.Xwin.Size = new System.Drawing.Size(131, 39);
+            this.Xwin.TabIndex = 14;
+            this.Xwin.Text = "label2";
+            this.Xwin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Xwin.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Owin
+            // 
+            this.Owin.ForeColor = System.Drawing.Color.ForestGreen;
+            this.Owin.Location = new System.Drawing.Point(619, 127);
+            this.Owin.Name = "Owin";
+            this.Owin.Size = new System.Drawing.Size(131, 39);
+            this.Owin.TabIndex = 15;
+            this.Owin.Text = "label3";
+            this.Owin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CKM
+            // 
+            this.CKM.Appearance = System.Windows.Forms.Appearance.Button;
+            this.CKM.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.CKM.BackgroundImage = global::game.Properties.Resources.light_neon_inscription_134108_1920x1080;
+            this.CKM.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CKM.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.CKM.ForeColor = System.Drawing.Color.LimeGreen;
+            this.CKM.Location = new System.Drawing.Point(827, 36);
+            this.CKM.Name = "CKM";
+            this.CKM.Size = new System.Drawing.Size(196, 110);
+            this.CKM.TabIndex = 16;
+            this.CKM.Text = "If you get bored just click on [ light ]\r\n\r\n\r\n\r\n\r\n";
+            this.CKM.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.CKM.UseCompatibleTextRendering = true;
+            this.CKM.UseVisualStyleBackColor = false;
+            this.CKM.CheckedChanged += new System.EventHandler(this.CKM_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = global::game.Properties.Resources._246223;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1054, 627);
+            this.Controls.Add(this.CKM);
+            this.Controls.Add(this.Owin);
+            this.Controls.Add(this.Xwin);
+            this.Controls.Add(this.DRAW);
+            this.Controls.Add(this.exB);
+            this.Controls.Add(this.REB);
+            this.Controls.Add(this.NGB);
+            this.Controls.Add(this.A20);
+            this.Controls.Add(this.A21);
+            this.Controls.Add(this.A22);
+            this.Controls.Add(this.A12);
+            this.Controls.Add(this.A11);
+            this.Controls.Add(this.A10);
+            this.Controls.Add(this.A02);
+            this.Controls.Add(this.A01);
+            this.Controls.Add(this.A00);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "THEDEAD GAME";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A00;
+        private System.Windows.Forms.Button A01;
+        private System.Windows.Forms.Button A02;
+        private System.Windows.Forms.Button A10;
+        private System.Windows.Forms.Button A11;
+        private System.Windows.Forms.Button A12;
+        private System.Windows.Forms.Button A22;
+        private System.Windows.Forms.Button A21;
+        private System.Windows.Forms.Button A20;
+        private System.Windows.Forms.Button NGB;
+        private System.Windows.Forms.Button REB;
+        private System.Windows.Forms.Button exB;
+        private System.Windows.Forms.Label DRAW;
+        private System.Windows.Forms.Label Xwin;
+        private System.Windows.Forms.Label Owin;
+        private System.Windows.Forms.CheckBox CKM;
+    }
+}
+
---
+++ game/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace game
+{
+    public partial class Form1 : Form
+    {
+        private SoundPlayer _soundPlayer;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            _soundPlayer = new SoundPlayer("C: \\ Users \\ THEDEAD \\ Documents \\ Visual Studio 2017 \\ game \\ 01 Beautiful People.wav");
+        }
+        public int player = 2;
+        public int turns = 0;
+        public int s1 = 0;
+        public int s2 = 0;
+        public int sd = 0;
+
+
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void buttonclick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Text == "")
+            {
+                if (player % 2 == 0)
+                {
+                    button.Text = "X";
+                    player++;
+                    turns++;
+                }
+                else
+                {
+                    button.Text = "O";
+                    player++;
+                    turns++;
+                }
+                if (checkdraw() ==true)
+                {
+                    MessageBox.Show("****Tie game****");
+                    sd++;
+                    Newgame();
+
+                }
+
+                if(checkwinner()==true )
+                {
+                    if(button.Text=="X")
+                    {
+                      ForeColor = (ForeColor == new Color()) ? Color.Black : ForeColor;
+                        MessageBox.Show(" {X} player  \n ****WON****");
+                        s1++;
+                        Newgame();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show(" {O} player \n ****WON****");
+                        s2++;
+                        Newgame();
+                    }
+                    
+                }
+
+            }
+
+        }
+
+        private void exB_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void REB_Click(object sender, EventArgs e)
+        {
+            s1 = s2 = sd = 0;
+            Newgame();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Xwin.Text = "| {X} | player :  " + s1;
+            Owin.Text = "| {O} | player  :  " + s2;
+            DRAW.Text = "|  {DRAW} |  :  " + sd;
+
+
+        }
+
+        void Newgame()
+        {
+            player = 2;
+            turns = 0;
+            A00.Text = A01.Text = A02.Text = A10.Text = A11.Text = A12.Text = A20.Text = A21.Text = A22.Text = "" ;
+            Xwin.Text = "| {X} | player :  " + s1;
+            Owin.Text = "| {O} | player  :  " + s2;
+            DRAW.Text = "|  {DRAW} |  :  " + sd;
+
+           
+        }
+
+        private void NGB_Click(object sender, EventArgs e)
+        {
+
+            Newgame();
+        }
+
+        bool checkdraw ()
+        {
+            if ((turns == 9)&&(checkwinner()==false))
+            {  return true;
+            }
+            else
+            {  return false;
+            }
+        }
+
+        bool checkwinner()
+        {
+            //horizontal check
+
+            if((A00.Text==A01.Text) && (A01.Text==A02.Text) && A00.Text !="")
+            {
+                return true;
+            }
+            else if ((A10.Text == A11.Text) && (A11.Text == A12.Text) && A10.Text != "")
+            {
+                return true;
+            }
+            else if ((A20.Text == A21.Text) && (A21.Text == A22.Text) && A20.Text != "")
+            {
+                return true;
+            }
+            //vertical check
+            if ((A00.Text == A10.Text) && (A10.Text == A20.Text) && A00.Text != "")
+            {
+                return true;
+            }
+            else if ((A01.Text == A11.Text) && (A11.Text == A21.Text) && A01.Text != "")
+            {
+                return true;
+            }
+            else if ((A02.Text == A12.Text) && (A12.Text == A22.Text) &&  A02.Text != "")
+            {
+                return true;
+            }
+             
+            if((A00.Text==A11.Text) && (A11.Text==A22.Text) && A00.Text != "")
+            {
+                return true;
+            }
+            else if ((A02.Text == A11.Text) && (A11.Text == A20.Text) && A02.Text != "")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+        private void CKM_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (CKM.Checked)
+            {
+                CKM.Text = " STOP";
+                _soundPlayer.Play();
+            }
+            else
+            {
+                CKM.Text = " PLAY";
+                _soundPlayer.Stop();
+            }
+        }
+    }
+}
